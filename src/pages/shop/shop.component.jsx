30c35c1278c12d8177484689d0eab5d9f67f02d3
Dawit Getachew <dawitgetachew3811@gmail.com>
2022-03-13 18:07:06 +0300

    adding header component and links to home, shop page and contact

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,6 +4,7 @@ import SHOP_DATA from './shop.data';
 
 import CollectionPreview from '../../component/collection-preview/collection-preview.component';
 
+
 class ShopPage extends React.Component{
     constructor(props){
         super(props);
